017cab34e20c73f408459cd8a7df645bac0b3b82
Clayton Grassick <clayton@claytronics.org>
2019-05-29 08:20:08 -0400

    TS fix

--- lib/PopoverHelpComponent.d.ts
+++ lib/PopoverHelpComponent.d.ts
@@ -1,5 +1,5 @@
 import { default as React, Component } from 'react'
 
 /** Shows a popover when help icon is clicked. Needs bootstrap */
-export default class PopoverHelpComponent extends Component<{ placement: "top" | "right" | "bottom" | "left" }> {
+export default class PopoverHelpComponent extends Component<{ placement?: "top" | "right" | "bottom" | "left" }> {
 }
