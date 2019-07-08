4a2b9baa5bb05989452b9be7bc6478caa72a0257
Clayton Grassick <clayton@claytronics.org>
2019-07-08 08:44:35 -0400

    Generate declarations

---
+++ lib/PopupHelpComponent.d.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+/** Shows a popup when help icon is clicked. Needs bootstrap */
+export default class PopoverHelpComponent extends React.Component<{}, {
+    open: boolean;
+}> {
+    constructor(props: {});
+    handleOpen: () => void;
+    handleClose: () => void;
+    render(): JSX.Element;
+}
