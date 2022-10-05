efc95afc486d8199f1062e17c4fb1f7b70f3ca10
Clayton Grassick <clayton@claytronics.org>
2022-10-05 08:32:23 -0400

    License fix, added HashHistory to reuse

---
+++ lib/HashHistory.d.ts
@@ -0,0 +1,57 @@
+/** Simple hash-based history that allows blocking transitions within the page
+ * asynchronously. */
+export declare class HashHistory {
+    /** Index of current page in stack. Used to determine if push or pop */
+    index: number;
+    /** Listeners for location changes */
+    locationListeners: HashLocationListener[];
+    /** Listeners for blockers */
+    blockerListeners: HashBlockerListener[];
+    /** Number of popstate events to ignore */
+    ignoringPopstate: number;
+    /** Current location */
+    currentLocation: HashLocation;
+    constructor();
+    /** Gets the current location */
+    getLocation(): HashLocation;
+    /** Convenience getter */
+    get location(): HashLocation;
+    /** Parse a location. Uri should not include initial "#" */
+    private parseLocation;
+    private handlePopstate;
+    /** Add listener to final changes in location. Returns unsubscribe */
+    addLocationListener(listener: HashLocationListener): () => void;
+    /** Add listener to block location changes. Returns unsubscribe */
+    addBlockerListener(listener: HashBlockerListener): () => void;
+    private notifyLocationListeners;
+    private checkBlockers;
+    /** Push a new location */
+    push(location: string): void;
+    push(location: {
+        pathname: string;
+        search?: string;
+    }): void;
+    /** Replace current location */
+    replace(location: string): void;
+    replace(location: {
+        pathname: string;
+        search?: string;
+    }): void;
+    /** Go back. Calls history.back() */
+    back(): void;
+}
+export interface HashLocation {
+    /** Base part of the path. Starts with "/" */
+    pathname: string;
+    /** query string. Includes ? if present */
+    search: string;
+    /** Hash part of hash url. Includes "#" if present */
+    hash: string;
+    /** Complete uri which includes all of the hash, starting with "/" */
+    uri: string;
+    /** Index of page in stack. Can be used to sense pushes/pops to same location */
+    index: number;
+}
+export declare type HashLocationListener = (location: HashLocation) => void;
+/** True if blocked, false to proceed */
+export declare type HashBlockerListener = (prevLocation: HashLocation, nextLocation: HashLocation) => Promise<boolean> | boolean;
---
+++ src/HashHistory.ts
@@ -0,0 +1,177 @@
+/** Simple hash-based history that allows blocking transitions within the page
+ * asynchronously. */
+ export class HashHistory {
+  /** Index of current page in stack. Used to determine if push or pop */
+  index: number
+
+  /** Listeners for location changes */
+  locationListeners: HashLocationListener[]
+
+  /** Listeners for blockers */
+  blockerListeners: HashBlockerListener[]
+
+  /** Number of popstate events to ignore */
+  ignoringPopstate: number
+
+  /** Current location */
+  currentLocation: HashLocation
+
+  constructor() {
+    this.locationListeners = []
+    this.blockerListeners = []
+    this.ignoringPopstate = 0
+
+    // Save index of 0 as current state and normalize path
+    this.index = 0
+    history.replaceState(0, "", window.location.hash || "#/")
+
+    this.currentLocation = this.getLocation()
+
+    // Listen for popstate
+    window.addEventListener("popstate", this.handlePopstate)
+  }
+
+  /** Gets the current location */
+  getLocation(): HashLocation {
+    const uri = (window.location.hash || "#").substr(1)
+    return this.parseLocation(uri, this.index)
+  }
+
+  /** Convenience getter */
+  get location(): HashLocation {
+    return this.getLocation()
+  }
+
+  /** Parse a location. Uri should not include initial "#" */
+  private parseLocation(uri: string, index: number) {
+    const match = uri.match(/^(.*?)(\?.*?)?(#.*)?$/)!
+    return {
+      pathname: match[1] || "/",
+      search: match[2] || "",
+      hash: match[3] || "",
+      uri: uri || "/",
+      index
+    }
+  }
+
+  private handlePopstate = async (ev: PopStateEvent) => {
+    if (this.ignoringPopstate > 0) {
+      this.ignoringPopstate--
+      return
+    }
+
+    // Determine delta
+    const delta = ev.state == null ? 1 : ev.state - this.index
+
+    // Check blockers
+    const blocked = await this.checkBlockers()
+    if (blocked) {
+      // Undo
+      this.ignoringPopstate++
+      history.go(-delta)
+      return
+    }
+
+    // Set index in state
+    this.index += delta
+    history.replaceState(this.index, "")
+
+    const location = this.getLocation()
+    this.currentLocation = location
+    this.notifyLocationListeners(location)
+  }
+
+  /** Add listener to final changes in location. Returns unsubscribe */
+  addLocationListener(listener: HashLocationListener): () => void {
+    this.locationListeners.push(listener)
+    return () => {
+      this.locationListeners = this.locationListeners.filter((l) => l != listener)
+    }
+  }
+
+  /** Add listener to block location changes. Returns unsubscribe */
+  addBlockerListener(listener: HashBlockerListener): () => void {
+    this.blockerListeners.push(listener)
+    return () => {
+      this.blockerListeners = this.blockerListeners.filter((l) => l != listener)
+    }
+  }
+
+  private notifyLocationListeners(location: HashLocation) {
+    for (const listener of this.locationListeners) {
+      listener(location)
+    }
+  }
+
+  private async checkBlockers(): Promise<boolean> {
+    for (const listener of this.blockerListeners) {
+      if (await listener(this.currentLocation, this.getLocation())) {
+        return true
+      }
+    }
+    return false
+  }
+
+  /** Push a new location */
+  push(location: string): void
+  push(location: { pathname: string, search?: string }): void
+  async push(location: string | { pathname: string, search?: string }) {
+    if (typeof location !== "string") {
+      location = location.pathname + (location.search || "")
+    }
+
+    if (await this.checkBlockers()) {
+      return
+    }
+
+    this.index += 1
+    history.pushState(this.index, "", "#" + location)
+
+    this.currentLocation = this.getLocation()
+    this.notifyLocationListeners(this.getLocation())
+  }
+
+  /** Replace current location */
+  replace(location: string): void
+  replace(location: { pathname: string, search?: string }): void
+  async replace(location: string | { pathname: string, search?: string }) {
+    if (typeof location !== "string") {
+      location = location.pathname + (location.search || "")
+    }
+
+    if (await this.checkBlockers()) {
+      return
+    }
+
+    history.replaceState(this.index, "", "#" + location)
+    this.currentLocation = this.getLocation()
+    this.notifyLocationListeners(this.getLocation())
+  }
+
+  /** Go back. Calls history.back() */
+  back() {
+    history.back()
+  }
+}
+
+export interface HashLocation {
+  /** Base part of the path. Starts with "/" */
+  pathname: string
+
+  /** query string. Includes ? if present */
+  search: string
+
+  /** Hash part of hash url. Includes "#" if present */
+  hash: string
+
+  /** Complete uri which includes all of the hash, starting with "/" */
+  uri: string
+
+  /** Index of page in stack. Can be used to sense pushes/pops to same location */
+  index: number
+}
+
+export type HashLocationListener = (location: HashLocation) => void
+
+/** True if blocked, false to proceed */
+export type HashBlockerListener = (prevLocation: HashLocation, nextLocation: HashLocation) => Promise<boolean> | boolean
